90dbcfdc5417dedb7d89d1fb4b83c20e812affb6
alexandrafofel <49486926+alexandrafofel@users.noreply.github.com>
2025-06-26 00:58:44 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0faab6bdbac9486fb" # Amazon Linux 2 pentru N. Virginia
+  ami           = "ami-0faab6bdbac9486fb"
   instance_type = "t2.micro"
-
-  subnet_id              = "subnet-0ea88fd8b99d74e57"
-  vpc_security_group_ids = []
-  key_name               = "brightfuture-key" # numele exact al cheii .pem din AWS
+  key_name      = "brightfuture-key"
 
   tags = {
     Name = "brightfuture-web"
